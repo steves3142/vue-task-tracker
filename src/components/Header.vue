ab92f3d6e42d3caf34174fbb3798b53166a12267
steves3142 <steves3142@gmail.com>
2023-08-27 00:14:14 -0400

    creating reminders with day and time i.e. vue component creation fest

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+/*scoped aka only for this component*/
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
